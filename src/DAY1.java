96073e81a6364bba835615abb2d7a89842cc45e6
414 <414@DESKTOP-3F8JT4N>
2018-07-03 16:32:05 +0900

    hi

---
+++ src/DAY1.java
@@ -0,0 +1,30 @@
+
+public class DAY1 {
+	  public static void main(String[] args)  {
+		   System.out.println("변수형들\n");
+		   
+		   int i = 10;
+		   int j = 20;
+		   int h = 0;
+		   char c1 = 'a';
+		   char c2 = 'B';
+		   char c3 = '^';
+		   
+		   char c ='씨';
+		   int a = 50;
+		   double d = 3.14;
+		   boolean b = true;
+		 
+		   System.out.println(i);
+		   System.out.println(j);
+		   System.out.println(h);
+		   System.out.println(c1);
+		   System.out.println(c2);
+		   System.out.println(c3 + "\n");
+		
+		   System.out.println("c는" +c);
+		   System.out.println("a는" +a);
+		   System.out.println("d는" +d);
+		   System.out.println("b는" +b);  
+ }
+}
